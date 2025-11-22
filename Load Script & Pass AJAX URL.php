5bc75a69bc0b97148974e939f8557ca2ec5d1940
Kaziyel Islam <155342603+Kaziyel-Islam@users.noreply.github.com>
2025-11-22 23:17:06 +0600

    Update Load Script & Pass AJAX URL.php

--- Load Script & Pass AJAX URL.php
+++ Load Script & Pass AJAX URL.php
@@ -1,5 +1,7 @@
+<?php
 wp_enqueue_script('woo-filter-js', get_template_directory_uri() . "/assets/js/woo-filter.js", array('jquery'), '1.0', true);
 
      wp_localize_script('woo-filter-js', 'ajax_object', [
         'ajax_url' => admin_url('admin-ajax.php')
     ]);
+?>
